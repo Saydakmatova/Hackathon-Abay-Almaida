4824393e45c350803d55764dbae7714ee8bea5ac
Saydakmatova <73686248+Saydakmatova@users.noreply.github.com>
2021-12-24 15:59:53 +0600

    finished

--- main.js
+++ main.js
@@ -30,6 +30,7 @@ async function addStudents(event) {
     };
     try {
         const response = await axios.post(API, newStudent);;
+        getStudent(API)
     } catch (error) {
         console.log(error);
     }
@@ -41,33 +42,33 @@ async function addStudents(event) {
 }
 addForm.on('submit', addStudents);
 
-let studentsList = $('.students-item');
+let studentsList = $('.students-list');
 let students = [];
 
-async function getStudenst(URL) {
+async function getStudent(URL) {
     try {
         const response = await axios(URL);
         students = response.data;
-        render(students)
+        handlePagination();
     } catch(error) {
         console.log(error);
     }
 }
-getStudenst(API);
+getStudent(API);
 
 function render(students) {
     studentsList.html('');
-    students.forEach((item, index) => {
+    students.forEach(item => {
         tbody.append(`
                     <tr>
-                        <td>${index + 1}</td>
+                        <td>${item.id}</td>
                         <td>${item.name}</td>
                         <td>${item.surname}</td>
                         <td>${item.phone}</td>
                         <td>100</td>
-                        <td>${item.js}</td>
+                        <td class="color-red">${item.js}</td>
                         <td>100</td>
-                        <td>${item.jq}</td>
+                        <td class="color-red"> ${item.jq}</td>
                         <td>${+item.js + +item.jq}/200</td>
                         <td>
                             <button class="btn-delete" id="${item.id}">
@@ -84,3 +85,125 @@ function render(students) {
         `)
     })
 }
+
+
+async function deleteStudent(e) {
+    let id = e.currentTarget.id;
+    let yes = confirm('Are You Sure?');
+    try {
+        if(yes) {
+            await axios.delete(`${API}/${id}`);
+            alert('Успешно удалено');
+            getStudent(API);
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+$(document).on('click', '.btn-delete', deleteStudent);
+
+
+let editInpName = $('.edit-inp-name');
+let editInpSurname = $('.edit-inp-surname');
+let editInpPhone = $('.edit-inp-phone');
+let editInpJs = $('.edit-inp-js');
+let editInpJq = $('.edit-inp-jq');
+let editForm = $('.edit-form');
+
+
+//! UPDATE
+
+async function getStudentEdit(e) {
+    let id = e.currentTarget.id;
+    try {
+        const responce = await axios(`${API}/${id}`);
+        editInpName.val(responce.data.name);
+        editInpSurname.val(responce.data.surname);
+        editInpPhone.val(responce.data.phone);
+        editInpJs.val(responce.data.js);
+        editInpJq.val(responce.data.jq);
+
+        editForm.attr('id', id);
+    } catch(error) {
+        console.log(error);
+    }
+
+}
+$(document).on('click', '.btn-edit', getStudentEdit);
+
+async function saveEditedStudent(e) {
+    e.preventDefault();
+    let id = e.currentTarget.id;
+    let editedStudent = {
+        name: editInpName.val().trim(),
+        surname: editInpSurname.val().trim(),
+        phone: editInpPhone.val().trim(),
+        js: editInpJs.val().trim(),
+        jq: editInpJq.val().trim()
+    }
+    for(let key in editedStudent) {
+        if(!editedStudent[key]) {
+            alert('Заполните поля!!!');
+            return;
+        }
+        try{
+            await axios.patch(`${API}/${id}`, editedStudent);
+            getStudent(API);
+            $('.modal').modal('hide');
+        } catch(error) {
+            console.log(error);
+        }
+    }
+}
+editForm.on('submit', saveEditedStudent);
+
+
+// ! Pagination
+
+let pagination = $(".pagination")
+const productsPerPage = 4;
+let currentPage = 1;
+let pagesCount = 1;
+
+function handlePagination() {
+    let indexOfLastProduct = currentPage * productsPerPage;
+    let indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+    let currentProducts = students.slice(indexOfFirstProduct, indexOfLastProduct);
+    render(currentProducts);
+    pagesCount = Math.ceil(students.length / productsPerPage);
+    addPagination(pagesCount)
+
+}
+function addPagination(pagesCount) {
+    pagination.html('');
+    for(let i = 1; i <= pagesCount; i++) {
+        pagination.append( `
+        <li class="page-item ${currentPage === i ? "active" : ''}">
+        <a class="page-link pagination-item" href="#" style = "background-color: #4ad295">${i}
+        </a></li>
+        `)
+    }
+    console.log(pagesCount)
+}
+$(document).on('click', '.pagination-item', (event) => {
+    let newPage = event.target.innerText;
+    currentPage = +newPage;
+    handlePagination();
+})
+
+// ! Search
+
+let searchInp = $('.search-inp');
+async function liveSearch(event) {
+    let value = event.target.value;
+    try {
+        const responce = await axios(`${API}?q=${value}`);
+        students = responce.data;
+        currentPage = 1
+        handlePagination(students)
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+searchInp.on('input', liveSearch);
